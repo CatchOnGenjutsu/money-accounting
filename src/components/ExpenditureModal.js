97607798a145b2c307485f8de3a39b3890c74509
CatchOnGenjutsu <kozlov.dmitriy.dev@gmail.com>
2022-08-11 11:13:52 +0300

    try to fix scale bug

--- src/components/ExpenditureModal.js
+++ src/components/ExpenditureModal.js
@@ -23,17 +23,17 @@ function ExpenditureModal({ hidden }) {
       setDecrementValue("");
     }
   }
-  useEffect(() => {
-    setTimeout(function () {
-      let viewheight = window.innerHeight;
-      let viewwidth = window.innerWidth;
-      let viewport = document.querySelector("meta[name=viewport]");
-      viewport.setAttribute(
-        "content",
-        `height=${viewheight}px, width=${viewwidth}px, initial-scale=1.0"`
-      );
-    }, 200);
-  }, []);
+  // useEffect(() => {
+  //   setTimeout(function () {
+  //     let viewheight = window.innerHeight;
+  //     let viewwidth = window.innerWidth;
+  //     let viewport = document.querySelector("meta[name=viewport]");
+  //     viewport.setAttribute(
+  //       "content",
+  //       `height=${viewheight}px, width=${viewwidth}px, initial-scale=1.0"`
+  //     );
+  //   }, 200);
+  // }, []);
 
   return (
     <div hidden={!hidden} className="overlay">
