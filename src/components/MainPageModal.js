39e82b508227fc221198d8f0c2f3f6a7cb8de17c
CatchOnGenjutsu <kozlov.dmitriy.dev@gmail.com>
2022-08-17 16:47:03 +0300

    tried to add no-scalable option

--- src/components/MainPageModal.js
+++ src/components/MainPageModal.js
@@ -69,7 +69,7 @@ function MainPageModal({ hidden, closeModal }) {
       let viewport = document.querySelector("meta[name=viewport]");
       viewport.setAttribute(
         "content",
-        `height=${viewheight}px, width=${viewwidth}px, initial-scale=1.0 user-scalable=no"`
+        `height=${viewheight}px, width=${viewwidth}px, initial-scale=1.0, user-scalable=no"`
       );
     }, 200);
   }, []);
